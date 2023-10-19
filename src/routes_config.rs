a5d0fd423a6d3fbef185e24bd80317b8e8c3b154
Karun Matharu <karun@coinroutes.com>
2023-10-19 10:21:38 +0000

    market depth additions to routes config

--- src/routes_config.rs
+++ src/routes_config.rs
@@ -1,10 +1,14 @@
 use phf::phf_map;
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub enum MarketDataType {
     CbboV1,
+    MarketDepthV1,
     RestCostCalculatorV1,
+    Direct, // for pass through requests, used on proxy only
 }
+
+//TODO The maps below may not be required?
 
 #[derive(Debug, Clone)]
 pub enum SubscriptionType {
@@ -15,11 +19,13 @@ pub enum SubscriptionType {
 pub static ROUTES: phf::Map<&'static str, MarketDataType> = phf_map! {
     // "/legacy-cbbo/:symbol" => Routes::CBBO,
     "/api/streaming/cbbo" => MarketDataType::CbboV1,
-    "/api/cost-calculator" => MarketDataType::CbboV1,
+    "/api/streaming/market_depth" => MarketDataType::MarketDepthV1,
+    "/api/cost_calculator" => MarketDataType::RestCostCalculatorV1,
 };
 
 pub static SUB_TYPE: phf::Map<&'static str, SubscriptionType> = phf_map! {
     // "/legacy-cbbo/:symbol" => Routes::CBBO,
     "/api/streaming/cbbo" => SubscriptionType::SUBSCRIPTION,
-    "/api/cost-calculator" => SubscriptionType::AUTHENTICATED_REST,
+    "/api/streaming/market_depth" => SubscriptionType::SUBSCRIPTION,
+    "/api/cost_calculator" => SubscriptionType::AUTHENTICATED_REST,
 };
