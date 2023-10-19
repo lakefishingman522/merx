5a5de4a44c0d75ca489f5fb148cf7d06db0d6286
Karun Matharu <karun@coinroutes.com>
2023-10-19 10:20:49 +0000

    add helper for cbag exchagne to market conversions

--- src/md_handlers/helper.rs
+++ src/md_handlers/helper.rs
@@ -5,3 +5,12 @@ pub fn cbag_market_to_exchange(market: &str) -> String {
         _ => market.to_lowercase(),
     }
 }
+
+pub fn exchange_to_cbag_market(exchange: &str) -> String {
+    match exchange.to_lowercase().as_str() {
+        "gdax" => String::from("COINBASE"),
+        "huobipro" => String::from("HUOBI"),
+        _ => exchange.to_uppercase(),
+    }
+}
+
