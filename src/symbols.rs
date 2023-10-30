d376540f32fb7ce919f0cd87cd10ab2b68f6ba97
Karun Matharu <karun@coinroutes.com>
2023-10-30 17:55:51 +0000

    struct for holding symbols and other currency pair oriented data in state

--- src/symbols.rs
+++ src/symbols.rs
@@ -1,20 +1,21 @@
 use chrono::prelude::*;
 use chrono::Duration;
+use log::info;
 use std::collections::HashMap;
 
 use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Serialize, Deserialize)]
-struct Currency {
+pub struct Currency {
     slug: String,
-    cbbo_sizes: Vec<CbboSize>,
+    pub cbbo_sizes: Vec<CbboSize>,
     default_min_size: String,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
-struct CbboSize {
+pub struct CbboSize {
     id: u32,
-    size: String,
+    pub size: String,
     usd_value: Option<f64>,
     currency: u32,
 }
@@ -25,25 +26,60 @@ struct Exchange {
     name: String,
     api_name: Option<String>,
     api_symbol: Option<String>,
-    maker_fee: Option<f64>,
-    taker_fee: Option<f64>,
-    tick_size: String,
+    maker_fee: Option<String>,
+    taker_fee: Option<String>,
+    tick_size: Option<String>,
     max_post_size: Option<String>,
-    min_qty_incr: String,
-    min_order_size: String,
+    min_qty_incr: Option<String>,
+    min_order_size: Option<String>,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct CurrencyPairsResponse {
-    slug: String,
-    target_currency: Currency,
+    pub slug: String,
+    pub target_currency: Currency,
     funding_currency: Currency,
     exchanges: Vec<Exchange>,
     tick_size: String,
     product_type: String,
 }
 
+#[derive(Default)]
 pub struct Symbols {
-    cbbo_sizes: HashMap<String, Vec<f64>>,
+    pub cbbo_sizes: HashMap<String, Vec<f64>>,
     time_validated: DateTime<Utc>,
+}
+
+impl Symbols {
+    // pub fn add_or_update_symbols(
+    //     &mut self,
+    //     currency_pairs_response_vec: &Vec<CurrencyPairsResponse>,
+    // )->Result<(), String> {
+    //     //TODO: this is done under a write lock, not good
+    //     let cbbo_sizes: HashMap<String, Vec<f64>> = HashMap::new();
+    //     for currency_pair in currency_pairs_response_vec {
+    //         let mut cbbo_sizes_vec = Vec::new();
+    //         for cbbo_size in &currency_pair.target_currency.cbbo_sizes {
+    //                 //convert to f64
+    //                 let cbbo_size = match cbbo_size.size.parse::<f64>() {
+    //                     Ok(cbbo_size) => cbbo_size,
+    //                     Err(e) => {
+    //                         info!("Unable to parse cbbo_size: {:?}", e);
+    //                         return Err("Unable to parse cbbo_size".to_string());
+    //                     }
+    //                 };
+    //                 cbbo_sizes_vec.push(cbbo_size);
+    //         }
+    //         self.cbbo_sizes.insert(currency_pair.slug.clone(), cbbo_sizes_vec);
+    //     }
+    //     self.cbbo_sizes = cbbo_sizes;
+    //     self.time_validated = Utc::now();
+    //     Ok(())
+    // }
+
+    pub fn add_or_update_symbols(&mut self, symbols: Symbols) -> Result<(), String> {
+        self.cbbo_sizes = symbols.cbbo_sizes.clone();
+        self.time_validated = Utc::now();
+        Ok(())
+    }
 }
