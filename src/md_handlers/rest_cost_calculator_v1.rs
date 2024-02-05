dc1a23e03dc0c4e0d9a9e8c9e73f6e31f1e7f6a5
lakefishingman522 <calvingill89@gmail.com>
2024-02-05 23:11:10 +0000

    fix: added cost_calculate get api, not cost-calculate

--- src/md_handlers/rest_cost_calculator_v1.rs
+++ src/md_handlers/rest_cost_calculator_v1.rs
@@ -1,11 +1,13 @@
+use std::collections::HashMap;
+
 use axum::{
     body::Body,
-    extract::{Json, OriginalUri},
-    http::Response,
+    extract::{OriginalUri, Query}, // Path,Json,
+    http::Response,                // , Uri Request,
     response::IntoResponse,
     Extension,
 };
-use reqwest::StatusCode;
+use reqwest::{Client, StatusCode};
 use serde::Deserialize;
 
 use crate::functions::URIs;
@@ -23,45 +25,57 @@ pub struct RestCostCalculatorV1RequestBody {
 }
 
 pub async fn handle_request(
-    OriginalUri(original_uri): OriginalUri,
-    Extension(_uris): axum::Extension<URIs>,
-    Json(_body): Json<RestCostCalculatorV1RequestBody>,
+    original_uri: OriginalUri,
+    // Path(currency_pair): Path<String>,
+    _uris: Extension<URIs>,
+    Query(params): Query<HashMap<String, String>>,
+    // req: Request<Body>,
+    // Json(_body): Json<RestCostCalculatorV1RequestBody>,
 ) -> impl IntoResponse {
     info!(
         "Received Authenticated REST Forward Request {}",
-        original_uri
+        original_uri.0
     );
-    // let target_url = format!("http://{}{}", cbag_uri, original_uri);
+    let uri = original_uri.0.clone();
+
+    let mut target_url = url::Url::parse(&format!(
+        "http://{}/cost-calculator/{}",
+        _uris.cbag_uri,
+        uri.path().trim_start_matches("/api/cost_calculator/")
+    ))
+    .unwrap();
+
+    for (key, value) in &params {
+        target_url.query_pairs_mut().append_pair(key, value);
+    }
+
+    info!("Authenticated REST Forward Request to {}", target_url);
 
     // Make a GET request to the target server
-    // let client = Client::new();
-    // let target_res = client.get(&target_url).send().await;
+    let client = Client::new();
+    let target_res = client.get(target_url).send().await;
 
-    // match target_res {
-    //     Ok(response) => {
-    //         // Extract the status code
-    //         let status = StatusCode::from_u16(response.status().as_u16()).unwrap();
-    //         // Extract the headers
-    //         let headers = response.headers().clone();
-    //         // Extract the body as bytes
-    //         let body_bytes = response.bytes().await.unwrap();
-    //         // Create an Axum response using the extracted parts
-    //         let mut axum_response = Response::new(Body::from(body_bytes));
-    //         *axum_response.status_mut() = status;
-    //         axum_response.headers_mut().extend(headers);
+    match target_res {
+        Ok(response) => {
+            // Extract the status code
+            let status = StatusCode::from_u16(response.status().as_u16()).unwrap();
+            // Extract the headers
+            let headers = response.headers().clone();
+            // Extract the body as bytes
+            let body_bytes = response.bytes().await.unwrap();
+            // Create an Axum response using the extracted parts
+            let mut axum_response = Response::new(Body::from(body_bytes));
+            *axum_response.status_mut() = status;
+            axum_response.headers_mut().extend(headers);
 
-    //         axum_response
-    //     }
-    //     Err(_err) => {
-    //         // Build a 404 response with a body of type `axum::http::response::Body`
-    //         Response::builder()
-    //             .status(StatusCode::NOT_FOUND)
-    //             .body(Body::from("Not Found"))
-    //             .unwrap()
-    //     }
-    // }
-    Response::builder()
-        .status(StatusCode::NOT_FOUND)
-        .body(Body::from("Not Found"))
-        .unwrap()
+            axum_response
+        }
+        Err(_err) => {
+            // Build a 404 response with a body of type `axum::http::response::Body`
+            Response::builder()
+                .status(StatusCode::NOT_FOUND)
+                .body(Body::from("Not Found"))
+                .unwrap()
+        }
+    }
 }
